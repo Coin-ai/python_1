43948209cbd591723585d700e5ee30df8e7c5b3d
Artem K <divian@list.ru>
2020-02-09 06:50:34 +0300

    lesson2.2 add input()

--- lesson2/lesson2.2.py
+++ lesson2/lesson2.2.py
@@ -1,10 +1,14 @@
 # Для списка реализовать обмен значений соседних элементов, т.е. Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
 # При нечетном количестве элементов последний сохранить на своем месте. Для заполнения списка элементов необходимо использовать функцию input().
 
-my_list=[2,1,4,3,6,5,8,7,9]
-size=len(my_list)-1
+user_list=[]
+
+while answer:=input('enter value (end list if empty): '):
+    user_list.append(answer)
+
+size=len(user_list)-1
 i=0
 while i<size:
-    my_list[i],my_list[i+1]=my_list[i+1],my_list[i]
+    user_list[i],user_list[i+1]=user_list[i+1],user_list[i]
     i+=2
-print(my_list)
+print(user_list)
